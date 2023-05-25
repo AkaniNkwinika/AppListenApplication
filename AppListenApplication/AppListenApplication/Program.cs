d42adae5a7df8b0d38d69d274e896ef4a821a194
Akani Nkwinika <AkaniN@blts.co.za>
2023-05-25 17:26:21 +0200

    added a queue name

--- AppListenApplication/AppListenApplication/Program.cs
+++ AppListenApplication/AppListenApplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,20 +16,39 @@ namespace AppListenApplication
       
             var configConnection = new ConnectionFactory()
             {
-                HostName = "localhost",
-                UserName = "guest",
-                Password = "guest",
-                Port = 15672
+                HostName = "localhost", //Server Host
+                UserName = "guest",     //Username
+                Password = "guest",     //password
+                Port = 15672            //port
             };
 
+            //Connecting to the RabbitMQ server
             using(var connection = configConnection.CreateConnection())
             using (var virtualConnection = connection.CreateModel())
             {
+                //Declare a queus
                 virtualConnection.QueueDeclare(queue: "",
                     durable: true,
                     autoDelete: false,
                     arguments: null,
                     exclusive: false);
+
+                var receiver = new EventingBasicConsumer(virtualConnection);
+                receiver.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var firstName = message.Replace("Hello my name is,", "");
+                    Console.WriteLine("Received message:" + message + "");
+                    Console.WriteLine("Hello " + firstName + ",I am your father!");
+                };
+                //consume messages
+                virtualConnection.BasicConsume(queue: "WongaAssessment", // Queue name
+                    autoAck:true,// Auto-acknowledgment
+                    consumer: receiver);
+
+                Console.WriteLine("Listening for messages!!!!!!!");
+                Console.ReadLine();
             }
 
         }
