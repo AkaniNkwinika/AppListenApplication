19c1124d9cdb31e3ce79f82af404d65202b14851
Akani Nkwinika <AkaniN@blts.co.za>
2023-05-25 16:26:25 +0200

    Adding a project to a repo

---
+++ AppListenApplication/AppListenApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppListenApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            var configConnection = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                Port = 15672
+            };
+
+
+        }
+    }
+}
